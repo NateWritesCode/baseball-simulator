93290435f890409261915b65a7907c5ce4db89e3
NateWritesCode <7214789+NateWritesCode@users.noreply.github.com>
2024-05-08 21:34:42 -0500

    Changes

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,120 +1,33 @@
 import type { ComponentProps, ReactNode } from "react";
 import { cva } from "styled-system/css";
 import { styled } from "styled-system/jsx";
+import { StyledComponent } from "styled-system/jsx";
 
 const button = cva({
    base: {
       alignItems: "center",
-      appearance: "none",
       borderRadius: "radius-md",
-      cursor: "pointer",
+      boxShadow: "shadow-xs",
       display: "inline-flex",
       justifyContent: "center",
-      minWidth: "[0]",
-      outline: "none",
-      transition: "transform 0.1s ease",
-      userSelect: "none",
-      verticalAlign: "middle",
-      whiteSpace: "nowrap",
-      _active: {
-         transform: "translate(1px, 1px)",
-      },
-      _disabled: {
-         cursor: "not-allowed",
-         pointerEvents: "none",
-      },
-   },
-   compoundVariants: [{ hierarchy: "link", css: { px: "0", py: "0" } }],
-   defaultVariants: {
-      hierarchy: "primary",
-      size: "sm",
+      _disabled: {},
+      _focus: {},
    },
    variants: {
       hierarchy: {
+         "link-gray": {},
+         "link-color": {},
          primary: {
-            bg: "colorPalette.600",
-            borderColor: "colorPalette.600",
+            bg: "brand.600",
+            borderColor: "brand.600",
             borderStyle: "solid",
             borderWidth: "1px",
-            boxShadow: "shadow-xs",
             color: "white",
-            _disabled: {
-               bg: "gray.light.100",
-               borderColor: "gray.light.200",
-               color: "gray.light.400",
-            },
-            _focus: {
-               boxShadow:
-                  "[0px 1px 2px 0px color-mix(in srgb, var(--colors-gray-light-900) 5%, transparent), 0px 0px 0px 4px color-mix(in srgb, var(--colors-color-palette-500) 24%, transparent)]",
-            },
-            _hover: {
-               bg: "colorPalette.700",
-               borderColor: "colorPalette.700",
-            },
          },
-         secondary: {
-            // bg: {_light: "white"},
-            bg: { _light: "white", _dark: "gray.dark.900" },
-            // borderColor: "colorPalette.300",
-            borderColor: { _light: "colorPalette.300", _dark: "gray.dark.900" },
-            borderStyle: "solid",
-            borderWidth: "1px",
-            boxShadow: "shadow-xs",
-            // color: "colorPalette.700",
-            color: { _light: "colorPalette.700", _dark: "gray.light.100" },
-            _disabled: {
-               // bg: "white",
-               // borderColor: "gray.light.200",
-               // color: "gray.light.400",
-               bg: { _light: "white", _dark: "gray.dark.900" },
-               borderColor: {
-                  _light: "gray.light.200",
-                  _dark: "gray.dark.900",
-               },
-               color: { _light: "gray.light.400", _dark: "gray.light.400" },
-            },
-            _focus: {
-               boxShadow: {
-                  _light:
-                     "[0px 1px 2px 0px color-mix(in srgb, var(--colors-gray-light-900) 5%, transparent), 0px 0px 0px 4px color-mix(in srgb, var(--colors-color-palette-500) 24%, transparent)]",
-                  _dark: "[0px 1px 2px 0px color-mix(in srgb, var(--colors-gray-dark-900) 5%, transparent), 0px 0px 0px 4px color-mix(in srgb, var(--colors-color-palette-500) 24%, transparent)]",
-               },
-            },
-            _hover: {
-               // bg: "colorPalette.50",
-               // color: "colorPalette.800",
-               bg: { _light: "colorPalette.50", _dark: "colorPalette.800" },
-               color: { _light: "colorPalette.800", _dark: "colorPalette.50" },
-            },
-         },
-         tertiary: {
-            // color: "colorPalette.600",
-            color: { _light: "colorPalette.600", _dark: "colorPalette.100" },
-            _disabled: {
-               // color: "gray.light.400",
-               color: { _light: "gray.light.400", _dark: "gray.light.400" },
-            },
-            _focus: {},
-            _hover: {
-               // bg: "colorPalette.50",
-               // color: "colorPalette.700",
-               bg: { _light: "colorPalette.50", _dark: "colorPalette.700" },
-               color: { _light: "colorPalette.700", _dark: "colorPalette.50" },
-            },
-         },
-         link: {
-            // color: "colorPalette.600",
-            color: { _light: "colorPalette.600", _dark: "colorPalette.100" },
-            _disabled: {
-               // color: "gray.light.400",
-               color: { _light: "gray.light.400", _dark: "gray.light.400" },
-            },
-            _focus: {},
-            _hover: {
-               // color: "colorPalette.700",
-               color: { _light: "colorPalette.700", _dark: "colorPalette.50" },
-            },
-         },
+         "secondary-color": {},
+         "secondary-gray": {},
+         "tertiary-color": {},
+         "tertiary-gray": {},
       },
       size: {
          sm: {
@@ -123,30 +36,10 @@ const button = cva({
             py: "spacing-lg",
             textStyle: "text.sm.semibold",
          },
-         md: {
-            gap: "spacing-xs",
-            px: "[10px]",
-            py: "[14px]",
-            textStyle: "text.sm.semibold",
-         },
-         lg: {
-            gap: "spacing-sm",
-            px: "[10px]",
-            py: "spacing-xl",
-            textStyle: "text.md.semibold",
-         },
-         xl: {
-            gap: "spacing-sm",
-            px: "spacing-lg",
-            py: "[18px]",
-            textStyle: "text.md.semibold",
-         },
-         "2xl": {
-            gap: "[10px]",
-            px: "spacing-xl",
-            py: "[22px]",
-            textStyle: "text.lg.semibold",
-         },
+         md: {},
+         lg: {},
+         xl: {},
+         "2xl": {},
       },
    },
 });
@@ -158,19 +51,23 @@ const StyledButton = styled("button", button, {
 type TType = typeof StyledButton;
 
 interface ButtonProps extends ComponentProps<TType> {
-   iconLeading?: ReactNode;
-   iconTrailing?: ReactNode;
+   leftIcon?: ReactNode;
+   rightIcon?: ReactNode;
 }
 
 const Button = (props: ButtonProps) => {
-   const { children, iconLeading, iconTrailing, ...rest } = props;
-   return (
-      <StyledButton {...rest}>
-         {iconLeading && iconLeading}
-         {props.children}
-         {iconTrailing && iconTrailing}
-      </StyledButton>
-   );
+   return <StyledButton>{props.children}</StyledButton>;
 };
 
 export default Button;
+
+// interface IProps extends TType {
+//    leftIcon?: React.ReactNode;
+//    rightIcon?: React.ReactNode;
+// }
+
+// const MyButton: IProps = (props) => {
+//    return <>{props.leftIcon}{props.children}</>;
+// };
+
+// export default MyButton;
